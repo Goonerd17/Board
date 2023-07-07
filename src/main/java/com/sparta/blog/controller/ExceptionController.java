3ccd4dc13e66b4388bb026c3e1c093ff247083ff
Sihwan Yu <tlghks1217@naver.com>
2023-07-08 04:52:42 +0900

    global valid

--- src/main/java/com/sparta/blog/controller/ExceptionController.java
+++ src/main/java/com/sparta/blog/controller/ExceptionController.java
@@ -3,8 +3,13 @@ package com.sparta.blog.controller;
 import com.sparta.blog.exceptions.ErrorReason;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class ExceptionController {
@@ -12,5 +17,18 @@ public class ExceptionController {
     public ResponseEntity<ErrorReason> illegalExHandle(IllegalArgumentException ie) {
         ErrorReason errorReason = new ErrorReason(HttpStatus.BAD_REQUEST, "400", ie.getMessage());
         return ResponseEntity.status(errorReason.getStatus()).body(errorReason);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException me) {
+        BindingResult bindingResult = me.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (FieldError fieldError : fieldErrors) {
+            stringBuilder.append(fieldError.getDefaultMessage());
+            stringBuilder.append(" ");
+        }
+        return ResponseEntity.badRequest().body(stringBuilder.toString());
     }
 }
--- src/main/java/com/sparta/blog/controller/UserController.java
+++ src/main/java/com/sparta/blog/controller/UserController.java
@@ -4,17 +4,11 @@ import com.sparta.blog.dto.SignupRequestDto;
 import com.sparta.blog.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@Slf4j
 @Controller
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -29,29 +23,13 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody @Valid SignupRequestDto signupRequestDto, BindingResult bindingResult) {
-
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        if(fieldErrors.size() > 0) {
-            for (FieldError fieldError : bindingResult.getFieldErrors()) {
-                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-            }
-            return ResponseEntity.badRequest().body("다시 입력해주세요");
-        }
+    public ResponseEntity<String> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         String message = userService.signup(signupRequestDto);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @PostMapping("/signup/secureadmin")
-    public ResponseEntity<String> signupAdmin(@RequestBody @Valid SignupRequestDto signupRequestDto, BindingResult bindingResult) {
-
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        if(fieldErrors.size() > 0) {
-            for (FieldError fieldError : bindingResult.getFieldErrors()) {
-                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-            }
-            return ResponseEntity.badRequest().body("다시 입력해주세요");
-        }
+    public ResponseEntity<String> signupAdmin(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         signupRequestDto.setAdmin(true);
         String message = userService.signup(signupRequestDto);
         return new ResponseEntity<>(message, HttpStatus.OK);
